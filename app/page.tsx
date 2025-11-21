bb0b52716f8e8573deea4342b90e3122ba03ec14
atzenhoffer <atzenhoffer@sio.ndlp>
2025-11-21 11:34:54 +0100

    shadcm theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
 "use client"
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { supabase } from '../lib/supabaseClient';
 
+
 export default function Home() {
+  const [question, setQuestion] = useState<any>(null);
+
   useEffect(() => {
     async function fetchQuestion() {
       const { data, error } = await supabase
         .from('question')
         .select('*')
-        .limit(1);
 
       if (error) console.error(error);
       else console.log(data);
@@ -18,5 +20,27 @@ export default function Home() {
     fetchQuestion();
   }, []);
 
-  return <h1>Bienvenue sur CyberQuiz</h1>;
+  async function fetchQuestion() {
+    const { data, error } = await supabase
+      .from('question')
+      .select('*');
+
+    if (error) console.error(error);
+    else setQuestion(data[0]); // On stocke la première question dans l’état
+  }
+
+  return (
+    <div>
+      <h1>Bienvenue sur CyberQuiz</h1>
+
+      {question ? (
+        <div>
+          <h2>{question.texte}</h2>
+        </div>
+      ) : (
+        <p>Chargement de la question...</p>
+      )}
+
+    </div>
+  );
 }
