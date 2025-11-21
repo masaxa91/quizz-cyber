921d960815e73731e1196117c437ada55032e62c
atzenhoffer <atzenhoffer@sio.ndlp>
2025-11-21 11:55:52 +0100

    Amélioration de l'affichage

--- app/page.tsx
+++ app/page.tsx
@@ -3,40 +3,50 @@
 import { useEffect, useState } from 'react';
 import { supabase } from '../lib/supabaseClient';
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Alert, AlertTitle, AlertDescription } from "@/components/ui/alert";
+
 
 export default function Home() {
   const [question, setQuestion] = useState<any>(null);
 
   useEffect(() => {
     async function fetchQuestion() {
-        const { data, error } = await supabase
-          .from('question')
-          .select('*');
+      const { data, error } = await supabase
+        .from('question')
+        .select('*');
 
-        if (error) console.error(error);
-        else setQuestion(data[0]); // On stocke la première question dans l’état
+      if (error) console.error(error);
+      else setQuestion(data[0]); // On stocke la première question dans l’état
 
-      
-      }
+
+    }
 
 
     fetchQuestion();
   }, []);
 
   return <div>
-  {
-    question ? (
-      <Card className="max-w-xl mx-auto mt-6">
-        <CardHeader>
-          <CardTitle>Question</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <p>{question.texte}</p>
-        </CardContent>
-      </Card>
-    ) : (
-      <p>Chargement de la question...</p>
-    )
-  }
+
+    <Alert className="bg-blue-50 border-blue-300 text-blue-800 max-w-xl mx-auto mt-6">
+      <AlertTitle className="text-xl font-semibold">Bienvenue sur CyberQuiz</AlertTitle>
+      <AlertDescription>
+        Un quiz pour tester vos connaissances en cybersécurité.
+      </AlertDescription>
+    </Alert>
+
+    {
+      question ? (
+        <Card className="max-w-xl mx-auto mt-6">
+          <CardHeader>
+            <CardTitle>Question</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>{question.texte}</p>
+          </CardContent>
+        </Card>
+      ) : (
+        <p>Chargement de la question...</p>
+      )
+    }
   </div>
 }
