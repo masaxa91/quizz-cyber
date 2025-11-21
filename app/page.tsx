39f7137e2bc5e8b02ec1b6eebaf1118aabebf058
atzenhoffer <atzenhoffer@sio.ndlp>
2025-11-21 11:05:26 +0100

    connexion  a la base de données

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
-import Image from "next/image";
+"use client"
+
+import { useEffect } from 'react';
+import { supabase } from '../lib/supabaseClient';
 
 export default function Home() {
-  return (
-    <div>
-      Hello
-    </div>
-  );
+  useEffect(() => {
+    async function fetchQuestion() {
+      const { data, error } = await supabase
+        .from('question')
+        .select('*')
+        .limit(1);
+
+      if (error) console.error(error);
+      else console.log(data);
+    }
+
+    fetchQuestion();
+  }, []);
+
+  return <h1>Bienvenue sur CyberQuiz</h1>;
 }
