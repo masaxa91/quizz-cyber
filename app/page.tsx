3e52c4ca3858247ad3d68460493c0be2175e2ddc
atzenhoffer <atzenhoffer@sio.ndlp>
2025-11-21 11:50:14 +0100

    Ajout des card pour question

--- app/page.tsx
+++ app/page.tsx
@@ -2,45 +2,41 @@
 
 import { useEffect, useState } from 'react';
 import { supabase } from '../lib/supabaseClient';
-
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function Home() {
   const [question, setQuestion] = useState<any>(null);
 
   useEffect(() => {
     async function fetchQuestion() {
-      const { data, error } = await supabase
-        .from('question')
-        .select('*')
+        const { data, error } = await supabase
+          .from('question')
+          .select('*');
 
-      if (error) console.error(error);
-      else console.log(data);
-    }
+        if (error) console.error(error);
+        else setQuestion(data[0]); // On stocke la première question dans l’état
+
+      
+      }
+
 
     fetchQuestion();
   }, []);
 
-  async function fetchQuestion() {
-    const { data, error } = await supabase
-      .from('question')
-      .select('*');
-
-    if (error) console.error(error);
-    else setQuestion(data[0]); // On stocke la première question dans l’état
+  return <div>
+  {
+    question ? (
+      <Card className="max-w-xl mx-auto mt-6">
+        <CardHeader>
+          <CardTitle>Question</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p>{question.texte}</p>
+        </CardContent>
+      </Card>
+    ) : (
+      <p>Chargement de la question...</p>
+    )
   }
-
-  return (
-    <div>
-      <h1>Bienvenue sur CyberQuiz</h1>
-
-      {question ? (
-        <div>
-          <h2>{question.texte}</h2>
-        </div>
-      ) : (
-        <p>Chargement de la question...</p>
-      )}
-
-    </div>
-  );
+  </div>
 }
